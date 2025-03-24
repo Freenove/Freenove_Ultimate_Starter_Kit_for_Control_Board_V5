fea6a7769e735077d96bfd499bee52666138c875
Eeason Shen <109536807+Vegetable-SYC@users.noreply.github.com>
2025-03-24 15:46:40 +0800

    update

--- Sketches/Sketch_15.1.1_Detect_the_temperature/Sketch_15.1.1_Detect_the_temperature.ino
+++ Sketches/Sketch_15.1.1_Detect_the_temperature/Sketch_15.1.1_Detect_the_temperature.ino
@@ -5,6 +5,8 @@
   Modification: 2024/08/05
 **********************************************************************/
 
+#define Voltage_Offset -0.48 // Measuring the offset voltage of the 5V power pin can improve the accuracy of temperature measurement results.
+
 void setup() {
   Serial.begin(9600);                 // Initialize the serial port, set the baud rate into 9600
   Serial.println("UNO is ready!");    // Print the string "UNO is ready!"
@@ -14,9 +16,9 @@ void loop() {
   // Convert analog value of A0 port into digital value
   int adcVal = analogRead(A0);
   // Calculate voltage
-  float v = adcVal * 5.0 / 1024;
+  float v = adcVal * (5.0 + Voltage_Offset) / 1024;
   // Calculate resistance value of thermistor
-  float Rt = 10 * v / (5 - v);
+  float Rt = 10 * v / ((5.0 + Voltage_Offset) - v);
   // Calculate temperature (Kelvin)
   float tempK = 1 / (log(Rt / 10) / 3950 + 1 / (273.15 + 25));
   // Calculate temperature (Celsius)
