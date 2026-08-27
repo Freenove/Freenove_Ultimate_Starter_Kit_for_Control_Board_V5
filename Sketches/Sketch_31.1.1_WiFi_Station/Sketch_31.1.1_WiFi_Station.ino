09a06dd02a398e0e3e679a265830e207f96cff62
Vegetable-SYC <109536807+Vegetable-SYC@users.noreply.github.com>
2026-08-27 17:11:27 +0800

    Update Sketch_31.1.1_WiFi_Station.ino

--- Sketches/Sketch_31.1.1_WiFi_Station/Sketch_31.1.1_WiFi_Station.ino
+++ Sketches/Sketch_31.1.1_WiFi_Station/Sketch_31.1.1_WiFi_Station.ino
@@ -2,7 +2,7 @@
   Filename    : Sketch_38.1.1_WiFi_Station
   Description : Connect to your router using control board 
   Auther      : www.freenove.com
-  Modification: 2024/08/05
+  Modification: 2026/08/27
 **********************************************************************/
 
 #include "WiFiS3.h"
@@ -10,16 +10,21 @@
 const char *ssid_Router     = "********"; //Enter the router name
 const char *password_Router = "********"; //Enter the router password
 
-void setup(){
+int status = WL_IDLE_STATUS;
+
+void setup() {
   Serial.begin(115200);
   delay(2000);
   Serial.println("Setup start");
-  WiFi.begin(ssid_Router, password_Router);
-  Serial.println(String("Connecting to ")+ssid_Router);
-  while (WiFi.status() != WL_CONNECTED){
+
+  while (status != WL_CONNECTED) {
+    status = WiFi.begin(ssid_Router, password_Router);
+  }
+  while (WiFi.localIP() == IPAddress(0, 0, 0, 0)) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("\nConnected, IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println("Setup End");
